14b30fb5bc252fde34a17afa85dbc57326e5faea
Lum <lumtech@yandex.ru>
2024-09-28 12:14:18 +0300

    plain image file list

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
 $path = "./";
-
+$files = scandir($path);
+foreach($files as $f){
+    $ext = strtolower(pathinfo($path . $f, PATHINFO_EXTENSION));
+    if (in_array($ext, [ "jpg" , "jpeg" , "png" , "gif" , "svg" , "webp"]) ){
+        print($f);
+        print("<br>");
+    }
+    
+}
 
