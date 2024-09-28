86d90dd48ff91d87708a973aea7567990a6998e1
Lum <lumtech@yandex.ru>
2024-09-28 12:33:14 +0300

    nice printf with size and modify date

--- index.php
+++ index.php
@@ -5,8 +5,14 @@ $files = scandir($path);
 foreach($files as $f){
     $ext = strtolower(pathinfo($path . $f, PATHINFO_EXTENSION));
     if (in_array($ext, [ "jpg" , "jpeg" , "png" , "gif" , "svg" , "webp"]) ){
-        print($f);
-        print("<br>");
+        //print($f);
+        $file = new SplFileInfo($path . $f);
+        $modify_date = date('Y-m-d H:i:s', $file->getMTime());
+        $filesize = $file->getSize();
+
+        print("<pre>");
+        printf("%-20.20s   %15s         [%s]", $path . $f , number_format($filesize, 0, ',', ' ') , $modify_date);
+        print("</pre>");
     }
     
 }
